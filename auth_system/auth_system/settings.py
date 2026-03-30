5b14dd464f81a32c871c5a93d1145cedb470a4f7
Bademos <curbademus@yandex.ru>
2026-03-30 15:26:25 +0300

    readmi.md adding

--- auth_system/auth_system/settings.py
+++ auth_system/auth_system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 ####################################################################
 ##########################CSRF disabling############################
 from django.views.decorators.csrf import csrf_exempt
@@ -27,8 +28,12 @@ CSRF_COOKIE_SECURE = False
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_NAME = 'csrftoken'
 ####################################################################
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -103,11 +108,11 @@ WSGI_APPLICATION = 'auth_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': '172.18.36.127',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME','postgres'),
+        'USER': os.getenv('DB_USER','postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
